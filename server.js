b8da00f7ef1576c6d58652761235000af5edcc0e
jvserivera <jose.rivera233@upr.edu>
2021-05-25 21:41:04 -0400

    setting db connection

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const connectDB =  require('./config/db');
 
 const app = express();
 
 // Single Endpoint for testing, just sending data to the browser using res.send
 app.get('/', (req,res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6655;
 
 app.listen(PORT, () => console.log('Server started on port ${PORT}'));
 
