d4389e507819436aa4a148540dcb822b9ff634a8
jvserivera <jose.rivera233@upr.edu>
2021-07-11 15:41:49 -0400

    Added post like and unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -88,7 +88,6 @@ router.get('/:id', auth, async (req, res) => {
 });
 
 
-
 // @route       DELETE api/posts/:id
 // @desc        Deletye post by ID
 // @access      Private
@@ -120,6 +119,68 @@ router.delete('/:id', auth, async (req, res) => {
 
 });
 
+
+// @route       Put api/posts/like/:id
+// @desc        Like a post
+// @access      Private
+
+router.put('/like/:id', auth, async(req, res) => {
+
+    try{
+
+    const post = await Post.findById(req.params.id);
+    if(!post){
+        return res.status(404).send({msg: ' Post not found '});
+    }
+    // check if the post has already been liked 
+    if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+        return res.status(400).json( {msg: 'Post already liked'});
+    }
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+
+
+    }catch(err){
+        console.error(err.message);
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({msg: ' Post not found '});
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+
+// @route       Put api/posts/unlike/:id
+// @desc        Unlike a post
+// @access      Private
+
+
+router.put('/unlike/:id', auth, async(req, res) => {
+
+    try{
+
+    const post = await Post.findById(req.params.id);
+    if(!post){
+        return res.status(404).send({msg: ' Post not found '});
+    }
+    if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+        return res.status(400).json( {msg: 'Post has not been liked. '});
+    }
+
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+    
+    }catch(err){
+        console.error(err.message);
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({msg: ' Post not found '});
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 
 module.exports = router;
